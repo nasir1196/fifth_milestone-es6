0fe2408adf684f6a02cfa2a9c10060f2a24cf8b9
Nasir Uddin <monforingnasir@gmail.com>
2021-02-01 20:41:52 +0600

    templete string

---
+++ module-22/templete.js
@@ -0,0 +1,12 @@
+const firstName = "Justine";
+const lastName = "TimberLack";
+const fullName = firstName + ' ' + lastName + " Is a good boy";
+console.log(fullName);
+const fullName2 = `${firstName} ${lastName} Is a good boy`;
+console.log(fullName2);
+const calculate = `${54 + 46} ${5}`;
+console.log(calculate);
+const multiline = `I love you 
+But now I don't love you
+No need wait Beily road`;
+console.log(multiline);
