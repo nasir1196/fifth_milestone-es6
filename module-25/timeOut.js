f5e498dc104c9f56f731c05d0b381402fd968141
Nasir Uddin <monforingnasir@gmail.com>
2021-02-05 22:28:23 +0600

    setTimeOut and setInterval

---
+++ module-25/timeOut.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+    console.log(22222); //set interval use for same work call more than more
+}, 2500);
+setTimeout(() => {
+    console.log(33333);
+}, 3000);
+console.log(44444);
+console.log(55555);
